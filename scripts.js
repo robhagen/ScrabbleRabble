c7d3695d168c48af828141e41f70e3afda05a9a9
Rob Hagen <57631113+robhagen@users.noreply.github.com>
2021-05-03 16:36:42 +0200

    Finished word and wordScore evaluation

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 const alphabet = {"a":1, "b":3, "c":5, "d":2, "e":1, "f":4, "g":3, "h":4, "i":1, "j":4, "k":3, "l":3, "m":3, "n":1, "o":1, "p":3, "q":10, "r":2, "s":2, "t":2, "u":4, "v":4, "w":5, "x":8, "y":8, "z":4}
-let button = document.getElementById("button");
-
 scores = alphabet;
-
-let word = document.getElementById("userInput").value;
-word = word.toLowerCase();
-
+let button = document.getElementById("button");
 let sum = 0;
-for (var i = 0; i < word.length; ++i) {
-    sum += scores[word.charAt(i)] || 0;
-}
 
 button.onclick = function() {
-    alert(sum);
+    let word = document.getElementById("userInput").value;
+    word = word.toLowerCase();
+    for (var i = 0; i < word.length; ++i) {
+        sum += scores[word.charAt(i)] || 0;
+    }
+    console.log("Word score is " + sum);
+    console.log("Word length is " + word.length);
+    sum = 0;
 };
